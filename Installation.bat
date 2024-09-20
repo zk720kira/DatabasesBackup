27b352ca35b7cf1fd858c44f5fc6cac5aca84f45
zk720 <81690899+zk720kira@users.noreply.github.com>
2024-09-20 09:37:50 +0200

    Update Installation.bat

--- Installation.bat
+++ Installation.bat
@@ -9,11 +9,15 @@ python -m ensurepip
 python -m pip install --upgrade pip
 
 REM Installation des packages pour le bon fonctionnements du script
-pip install -r requirements.txt
+pip install -r C:\DatabasesBackup\requirements.txt
 
 REM Créer la tâche planifiée
 schtasks /create /tn "DatabasesBackup" /xml "C:\DatabasesBackup\DatabasesBackup.xml"
 
+REM Démarrer le script de backup
+schtasks /run /tn "DatabasesBackup"
+
 echo L'installation est terminee.
+echo Le script a ete demarre.
 echo Pressez sur Enter pour fermer la fenetre.
 PAUSE
