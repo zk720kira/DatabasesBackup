fc09b8e33cc4d2d5e404cea33fc1f40fa7e492e9
zk720 <81690899+zk720kira@users.noreply.github.com>
2024-09-20 09:11:40 +0200

    Update Installation.bat

--- Installation.bat
+++ Installation.bat
@@ -1,15 +1,18 @@
 @echo off
 cls
 
-REM Se d�placer dans le C:\
+REM Se déplacer dans le C:\
 cd C:\DatabasesBackup
 
-REM Installer les d�pendances python
+REM Installer les dépendances python
 python -m ensurepip
 python -m pip install --upgrade pip
 
 REM Installation des packages pour le bon fonctionnements du script
 pip install -r requirements.txt
+
+REM Créer la tâche planifiée
+schtasks /create /tn "DatabasesBackup" /xml "C:\DatabasesBackup\DatabasesBackup.xml"
 
 echo L'installation est terminee.
 echo Pressez sur Enter pour fermer la fenetre.
